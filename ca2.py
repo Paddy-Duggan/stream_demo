9140697e8b0db570b8094ee12edb7c9287df1ce8
Paddy-Duggan <sba24138@student.cct.ie>
2024-12-29 16:35:03 +0000

    Update ca2.py

--- ca2.py
+++ ca2.py
@@ -12,3 +12,17 @@ import plotly.express as px
 df = pd.read_csv('streamlit.csv')
 
 st.title('Organic Farming EU')
+fig = px.choropleth(
+    df,
+    locations='Country', 
+    animation_frame="Year",
+    locationmode='country names',
+    color='%_area_under_organic_farming',
+    hover_name='Country',
+    title='Cluster Distribution of Organic Farming Indices (2022)',
+    color_continuous_scale='viridis'
+)
+fig.update_layout(
+    title_text="% UAA under organic agriculture 2013-2022",
+    geo_scope="europe")
+fig.show()
